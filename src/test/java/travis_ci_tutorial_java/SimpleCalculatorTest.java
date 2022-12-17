ed47ff8388a0e6c397de4d8273ca2f0a2a1c152a
MaybeKapil <117200663+MaybeKapil@users.noreply.github.com>
2022-12-17 11:02:56 -0500

    Added features of multiply and divide.

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,5 +11,13 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 		assertEquals(calc.minus(5, 3), 2);
+		assertEquals(calc.multiply(-1, 1), -1);
+		assertEquals(calc.multiply(1, 1), 1);
+		assertEquals(calc.multiply(1, 0), 0);
+		assertEquals(calc.multiply(0, 0), 0);
+		assertEquals(calc.divide(1, 1), 1);
+		assertEquals(calc.divide(2, 1), 2);
+		assertEquals(calc.divide(1, 2), 0);
+		assertEquals(calc.divide(9, 10), 0);
 	}
 }
